22b016bae6b1d0803169ba589718c57a43a8765a
HXL1111 <2521556749@qq.com>
2022-09-30 16:16:02 +0800

    test

--- HXL-com/public/HXL.js
+++ HXL-com/public/HXL.js
@@ -1,10 +1,11 @@
 const request = new XMLHttpRequest()
-request.open("GET","http://qq.com:8888/friends.json")
+request.open("GET","http://localhost:8888/friends.json")
 
 request.onreadystatechange=()=>{
     
     if(request.readyState===4 ){
         console.log(request.readyState)
+        console.log(request.status)
         if(request.status >= 200 && request.status<300){
             console.log(request.status)
             console.log(request.response)
