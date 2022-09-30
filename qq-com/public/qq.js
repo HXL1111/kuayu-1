e1bd8b5db7a4f0e2e52990165b5bbd2972ffb117
HXL1111 <2521556749@qq.com>
2022-09-30 19:48:40 +0800

    test1

--- qq-com/public/qq.js
+++ qq-com/public/qq.js
@@ -1,10 +1,10 @@
-const request = new XMLHttpRequest()
-request.open("GET","/friends.json")
-request.onreadystatechange=()=>{
-    if(request.readyState===4 ){
-        if(request.status >= 200 && request.status<300){
-            console.log(request.response)
-        }
+const request = new XMLHttpRequest();
+request.open("GET", "/friends.json");
+request.onreadystatechange = () => {
+  if (request.readyState === 4) {
+    if (request.status >= 200 && request.status < 300) {
+      console.log(request.response);
     }
-}
-request.send()
+  }
+};
+request.send();
