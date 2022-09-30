b05be679b009b53f21ab5c13a84dad69aac355a1
HXL1111 <2521556749@qq.com>
2022-09-30 19:47:26 +0800

    test1

--- HXL-com/public/HXL.js
+++ HXL-com/public/HXL.js
@@ -1,15 +1,10 @@
-const request = new XMLHttpRequest()
-request.open("GET","http://localhost/friends.json")
-
-request.onreadystatechange=()=>{
-    
-    if(request.readyState===4 ){
-        console.log(request.readyState)
-        console.log(request.status)
-        if(request.status >= 200 && request.status<300){
-            console.log(request.status)
-            console.log(request.response)
-        }
+const request = new XMLHttpRequest();
+request.open("GET", "http://qq.com:8888/friends.json");
+request.onreadystatechange = () => {
+  if (request.readyState === 4) {
+    if (request.status >= 200 && request.status < 300) {
+      console.log(request.response);
     }
-}
-request.send()
+  }
+};
+request.send();
